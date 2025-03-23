53be06d5ff541be9f4febbb8efa8052086f25d32
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2025-03-24 00:21:13 +0900

    feat: initial commit

---
+++ YmmRPCPluginSorce.cs
@@ -0,0 +1,38 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using YukkuriMovieMaker.Plugin;
+
+namespace YmmRPC
+{
+    [PluginDetails(AuthorName = "namakemono-san", ContentId = "")]
+    public class YmmRpcPlugin : IPlugin
+    {
+        public string Name => "YMM-RPC";
+        
+        static YmmRpcPlugin()
+        {
+            var client = new DiscordRpcClient("1353376132732420136");			
+	
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+
+            client.OnReady += (sender, e) =>
+            {
+                Console.WriteLine("Received Ready from user {0}", e.User.Username);
+            };
+		
+            client.OnPresenceUpdate += (sender, e) =>
+            {
+                Console.WriteLine("Received Update! {0}", e.Presence);
+            };
+	
+            client.Initialize();
+            
+            // TODO: 作業内容を表示したい。
+            // client.SetPresence(new RichPresence()
+            // {
+            //     Details = "test123",
+            //     State = "アイドル中",
+            // });	
+        }
+    }
+}
